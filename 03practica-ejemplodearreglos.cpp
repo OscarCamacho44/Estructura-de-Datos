b65eb63f15e98637fec4cf77bf5bafe7a23421c6
OscarCamacho44 <144480683+OscarCamacho44@users.noreply.github.com>
2023-10-02 15:19:16 -0600

    Create 03practica-ejemplodearreglos.cpp
    03practica

---
+++ 03practica-ejemplodearreglos.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int numero[6] = { 10, 20, 30, 40, 50, 60 };
+    cout << numero[0];
+    cout << "\n";
+    cout << numero[1];
+    cout << "\n";
+    cout << numero[2];
+    cout << "\n";
+    cout << numero[3];
+    cout << "\n";
+    cout << numero[4];
+    cout << "\n";
+    cout << numero[5];
+    cout << "\n";
+    int n[3] = { 3, 4, 5 };
+    cout << n[0];
+    cout << "\n";
+    cout << n[1];
+    cout << "\n";
+    cout << n[2];
+    cout << "\n";
+
+    char c[] = { 'L' , 'U', 'i','S' };
+    cout << c[0];
+    cout << "\n";
+    cout << c[1];
+    cout << "\n";
+    cout << c[2];
+    cout << "\n";
+    cout << c[3];
+    cout << "\n";
+    char s[] = "mona lisa";
+    cout << s[0];
+    cout << s[1];
+    cout << s[2];
+    cout << s[3];
+    cout << s[4];
+    cout << s[5];
+    cout << s[6];
+    cout << s[7];
+    cout << s[8];
+    cout << "\n";
+    return 0;
+}
